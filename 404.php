1364908df21dec825dae9b8da5e7ce50eac4947e
Bogdan Bryzh <bogdan.bryzh@gmail.com>
2021-01-27 14:46:25 +0200

    removed 404 status (temporary)

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?
 include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
 
-CHTTP::SetStatus("404 Not Found");
-@define("ERROR_404","Y");
-define("HIDE_SIDEBAR", true);
+// CHTTP::SetStatus("404 Not Found");
+// @define("ERROR_404","Y");
+// define("HIDE_SIDEBAR", true);
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
