f3b49db1842b8b3f2d221da517ada8b323af4a83
Bogdan Bryzh <bogdan.bryzh@gmail.com>
2021-01-27 14:47:26 +0200

    fixed help link 404

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@ $APPLICATION->SetTitle("Страница не найдена");?>
             <div class="col-lg-1"></div>
             <div class="col-lg-5"><h1 class="error_404">404</h1>
                 <p class="error_description">Ошибка 404: страница не найдена. Выберете нужную
-                    в меню или воспользуйтесь разделом <a href="#">Помощь</a></p>
+                    в меню или воспользуйтесь разделом <a href="/help">Помощь</a></p>
                 <a href="/" class="catalog main_a">На главную &nbsp; <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path class="shop" d="M1 1L6 6.07081L1 11" stroke="#202945"></path>
                     </svg>
